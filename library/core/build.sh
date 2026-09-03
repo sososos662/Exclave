18855ed62a5b0848df925f656037310074bc9158
sososos662 <hp.petrenko007@gmail.com>
2026-09-04 00:54:07 +0300

    core: fetch fork modules direct from git (wsDBG build)

--- library/core/build.sh
+++ library/core/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# fronting fork: fetch our Go modules straight from git (no proxy lag),
+# allow go to record their hashes without sumdb
+export GOPRIVATE=github.com/sososos662/*
+export GOFLAGS=-mod=mod
+
+
 CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -buildid=" -tags="with_clash" "github.com/exclavenetwork/libexclavecore" || exit 1
 
 proj=../../app/libs
