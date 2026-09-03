36a36aa79d17b228b5cd398cb4a8ca211a4777cc
sososos662 <hp.petrenko007@gmail.com>
2026-09-04 01:04:11 +0300

    core: revert build.sh workaround (pseudo-version resolves via proxy)

--- library/core/build.sh
+++ library/core/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# fronting fork: fetch our Go modules straight from git (no proxy lag).
-export GOPRIVATE=github.com/sososos662/*
-# NOTE: GOFLAGS must NOT stay exported: gomobile's internal tidy breaks.
-# Record missing go.sum entries explicitly, then unset.
-export GOFLAGS=-mod=mod
-go mod tidy
-unset GOFLAGS
-
 
 CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -ldflags="-s -buildid=" -tags="with_clash" "github.com/exclavenetwork/libexclavecore" || exit 1
 
